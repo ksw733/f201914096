6974bec68bdca8e1b32f4e6a6493c3e5ffb00228
Kim, Seokwan <sukwan1106@gmail.com>
2023-10-18 10:47:02 +0900

    two

--- src/f201914096/Main.java
+++ src/f201914096/Main.java
@@ -2,6 +2,7 @@ package f201914096;
 
 public class Main {
 	public static void main(String[] args) {
+		Two.print();
 		System.out.println("one 201914096");
 	}
 }
---
+++ src/f201914096/Two.java
@@ -0,0 +1,8 @@
+package f201914096;
+public class Two {
+    public static void print() {
+        System
+            .out
+            .println("two");
+    }
+}
